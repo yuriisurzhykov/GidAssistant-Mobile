c2080e321350f7065de609acc032528c764ab512
yuriysurzhikov <yuriy.surzhickov@yandex.ua>
2020-10-24 23:42:27 +0300

    First settings for project

--- app/src/main/java/com/yuriysurzhikov/gidassistant/ui/MainActivity.kt
+++ app/src/main/java/com/yuriysurzhikov/gidassistant/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yuriysurzhikov.gidassistant
+package com.yuriysurzhikov.gidassistant.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yuriysurzhikov.gidassistant.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
