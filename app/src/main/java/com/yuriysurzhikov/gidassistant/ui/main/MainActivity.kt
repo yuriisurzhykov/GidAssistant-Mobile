3622d7a14d951d5bf9f4e0eadd7527f3abbf92c3
yuriysurzhikov <yuriy.surzhickov@yandex.ua>
2020-10-28 21:12:09 +0200

    Delete from mainActivity

--- app/src/main/java/com/yuriysurzhikov/gidassistant/ui/main/MainActivity.kt
+++ app/src/main/java/com/yuriysurzhikov/gidassistant/ui/main/MainActivity.kt
@@ -5,9 +5,5 @@ import android.os.Bundle
 import com.yuriysurzhikov.gidassistant.R
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 }
 
